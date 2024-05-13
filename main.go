a1ff48c46150ae483b6016a86771e2c290c116ce
assilbekov <askhat.assilbekov@gmail.com>
2024-05-13 20:24:50 -0300

    add implementation

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"mediator/pkg"
+	"time"
+)
+
+func main() {
+	stationManager := pkg.NewStationManager()
+	passengerTrain := &pkg.Passenger{stationManager}
+	cargoTrain := &pkg.Cargo{stationManager}
+
+	passengerTrain.Arrive()
+	time.Sleep(time.Second * 1)
+	cargoTrain.Arrive()
+	passengerTrain.Depart()
+	cargoTrain.Depart()
+	time.Sleep(time.Second * 1)
+
+	passengerTrain.Arrive()
+	time.Sleep(time.Second * 1)
+	cargoTrain.Arrive()
+}
